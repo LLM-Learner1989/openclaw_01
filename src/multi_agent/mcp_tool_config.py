f80d1ebdf3578484c3343012c03b85efbb8c1603
LLM-Learner1989 <zhangbiaohpu@outlook.com>
2026-09-09 12:10:12 +0800

    Update sandbox

--- src/multi_agent/mcp_tool_config.py
+++ src/multi_agent/mcp_tool_config.py
@@ -2,7 +2,7 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 
 # 数据分析报表的MCP服务端（工具的配置）
 analysis_mcp_server_chat_config = {
-    "url": "https://mcp.api-inference.modelscope.net/41cbff7f0b934c/mcp",
+    "url": "https://mcp.api-inference.modelscope.net/5ce3261da36841/mcp",
     "transport": "streamable_http",
 }
 
--- src/sandbox/opensandbox_opt.py
+++ src/sandbox/opensandbox_opt.py
@@ -40,6 +40,7 @@ def get_or_create_sandbox(config, sandbox_id=None, image=None):
         resource={"cpu": "4", "memory": "4Gi"},
         timeout=timedelta(minutes=30),  # 沙箱的空闲超时时间
         connection_config=config,
+        ready_timeout=timedelta(seconds=120),
         # network_policy=NetworkPolicy(  # 沙箱网络路由限制策略
         #     defaultAction="deny",
         #     egress=[
@@ -245,7 +246,7 @@ def sync_skills_to_sandbox(backend, local_skills_path, sandbox_skills_path):
 # 配置连接
 config = ConnectionConfigSync(
     domain="http://172.16.9.130:8080",
-    use_server_proxy=True,
+    use_server_proxy=False,
     request_timeout=timedelta(seconds=60),
     transport=httpx.HTTPTransport(limits=httpx.Limits(max_connections=20)),
 )
